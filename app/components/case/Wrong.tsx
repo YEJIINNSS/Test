e5e9a28b62e5ce9a5fe2ba5ee9fb929a8f565d18
YEJIINNSS <sarah011013@naver.com>
2024-08-09 01:52:30 +0900

    test

--- app/components/case/Wrong.tsx
+++ app/components/case/Wrong.tsx
@@ -13,10 +13,10 @@ function Wrong({
       <>
         <div className="fixed w-full h-screen bg-[#FF0017] opacity-20 -z-10" />
         <div className="flex flex-col items-center justify-center w-[60%] bg-white opacity-90 rounded-lg mb-24 p-6">
-          <span className="font-bold"> <br /> Please try again!</span>
           <span className="text-s whitespace-pre-line w-full text-center">
             { text }
           </span>
+          <span className="font-bold"> <br /> Please try again!</span>
           <Image
             src="/wrong.png"
             alt="wrong"
