926fa423aa271ce9c4c0af3c48043b859943fabf
YEJIINNSS <sarah011013@naver.com>
2024-08-08 18:45:46 +0900

    test

--- app/team/5/case/page.tsx
+++ app/team/5/case/page.tsx
@@ -175,7 +175,7 @@ export default function Case1() {
               Okay. who will be taking the medicine?
             </span>
           </div>
-          <div className="flex flex-col items-center w-full gap-5 justify-between mt-14">
+          <div className="flex flex-col justify-center w-full gap-5 justify-between mt-14">
             {TEAM_5_PATIENT.map((text, index) => (
             <div className="shadow-lg opacity-90">
               <BackBtn handleClick = {() => handleBackBtn(1)}/>
@@ -203,7 +203,7 @@ export default function Case1() {
               Choose the best treatment options.
             </span>
           </div>
-          <div className="flex flex-row items-center w-full gap-5 justify-between mt-14">
+          <div className="flex flex-row items-center justify-center w-full gap-5 justify-between mt-14">
             {TEAM_5_PROUDCT.map((product, index) => (
             <div className="shadow-lg opacity-90">
    
