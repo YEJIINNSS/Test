3e6b2de621136a8d790c25e0e77b95fe6dafb3d3
YEJIINNSS <sarah011013@naver.com>
2024-08-09 14:51:38 +0900

    test

--- app/team/5/case/page.tsx
+++ app/team/5/case/page.tsx
@@ -538,9 +538,9 @@ export default function Case1() {
             answer={TEAM_5_FINALRESULT_ANSWER[0]}
           />
 
-          <Link href="/team/5/otc-info" className="fixed left-10 top-10">
+          <Link href="/" className="fixed left-10 top-10">
             <div className="bg-[#5BC17F] px-5 py-2 rounded-full">
-              <span className="text-white">Go Back to Info Page</span>
+              <span className="text-white">Go Back to Main Page</span>
             </div>
           </Link>
         </>
